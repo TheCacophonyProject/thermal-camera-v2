ac1cd2c7e4d14daa935fbba1edbb8a5d1af75807
Cameron <cameron.ryan.pears@gmail.com>
2026-08-18 09:28:50 +1200

    Add 2D barcode for microphone

--- tc2-mic-panel.py
+++ tc2-mic-panel.py
@@ -13,10 +13,10 @@ command = [
     "--tabs", "annotation",
     "--cuts", "vcuts",
     "--post", "millradius: 1mm",
-    "--framing", "frame; cuts: none",
+    "--framing", "frame; cuts: both",
     "--fiducials", "type: 4fid; hoffset: 3.85mm; voffset:6mm",
     "--tooling", "type: 4hole; hoffset: 3mm; voffset: 3mm; size: 2mm",
-    "--copperfill", "type: solid; layers: all",
+    # "--copperfill", "type: solid; layers: all",
     "tc2-mic-pcb/tc2-mic-pcb.kicad_pcb",
     f"{dir_path}/tc2-mic-panel.kicad_pcb"
 ]
